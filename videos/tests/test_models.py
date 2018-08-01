d66ac07f7ad552b43b6805e2c1ff4fd47b4ebce0
Rob Charlwood <robcharlwood@gmail.com>
2018-08-01 20:35:14 +0100

    Added failing test for soon to be added VideoComment model

--- videos/tests/test_models.py
+++ videos/tests/test_models.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
 from djangae.test import TestCase
 
-from core.tests.factories import VideoFactory
+from core.tests.factories import VideoCommentFactory, VideoFactory
 
 
 class VideoModelTestCase(TestCase):
     def test_unicode_method(self):
         video = VideoFactory.create(name=u'象は鼻が長')
         self.assertEqual(video.__unicode__(), u'象は鼻が長')
+
+
+class VideoCommentTestCase(TestCase):
+    def test_unicode_method(self):
+        video = VideoFactory()
+        comment = VideoCommentFactory.create(video=video, comment=u'象は鼻が長')
+        self.assertEqual(
+            comment.__unicode__(), u'Video: {} Comment: 象は鼻が長'.format(
+                video.pk))
