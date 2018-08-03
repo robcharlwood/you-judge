a3cf15cc67ffe712702a147981aa686ced2b52d9
Rob Charlwood <robcharlwood@gmail.com>
2018-08-03 18:34:52 +0100

    Added method to perform sentiment analysis on passed text. All failing TDD tests from previous commit now passing

--- services/cloud_nl.py
+++ services/cloud_nl.py
@@ -8,8 +8,22 @@ class Client(object):
     Wrapper around the cloud natural language API
     """
     def __init__(self, service=None):
-        if service is None:
+        if service is None:  # pragma: no cover
             service = discovery.build(
                 'language', 'v1',
                 developerKey=settings.CLOUD_NATURAL_LANG_API_KEY)
         self.service = service
+
+    def analyze_sentiment(self, text, lang='en', ctype='PLAIN_TEXT'):
+        """
+        Takes a string and performs setiment anaylsis on it using google's
+        cloud service
+        """
+        results = self.service.documents().analyzeSentiment(body={
+            'document': {
+                'language': lang,
+                'content': text,
+                'type': ctype,
+            }
+        }).execute()
+        return results
