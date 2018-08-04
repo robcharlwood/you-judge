0748545ad7e0cbc9c709684b816b696999d8a0a5
Rob Charlwood <robcharlwood@gmail.com>
2018-08-04 15:51:04 +0100

    Updated video post save signal handler to kick off task to grab video transcript. All failing TDD tests now passing

--- videos/signals.py
+++ videos/signals.py
@@ -2,7 +2,7 @@ import logging
 
 from google.appengine.ext import deferred
 
-from .tasks import youtube_import_comments
+from .tasks import youtube_import_comments, youtube_import_transcript
 
 logger = logging.getLogger(__name__)
 
@@ -14,4 +14,6 @@ def import_youtube_comments(sender, instance, created=None, **kwargs):
     """
     if created:
         logger.info('Retrieving comments for YouTube video %r', instance.pk)
-        deferred.defer(youtube_import_comments, instance.pk, _queue='youtube')
+        deferred.defer(youtube_import_comments, instance.pk, _queue='comments')
+        logger.info('Retrieving transcript for YouTube video %r', instance.pk)
+        deferred.defer(youtube_import_transcript, instance.pk, _queue='videos')
